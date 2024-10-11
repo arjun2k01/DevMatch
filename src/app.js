8901f35e45394f36c5427461585a20637ea823ec
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2024-10-11 22:49:50 +0530

    now i learned about db

--- src/app.js
+++ src/app.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const connectDB = require('./config/database')
 const app = express();
-const User = require('./models/user');
+const UserModel = require('./models/user');
 app.use(express.json());
-app.post('/signup', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.send('User created successfully')
-    } catch (err) {
-        res.status(400).send(err)
-    }
 
+app.post('/signup', async (req, res) => {
+  
+    //creating a new user obj
+    const user = new UserModel(req.body);
+    try {
+        await user.save();
+        res.send('User created successfully');
+    } catch (err) { 
+        res.status(400).send(err.message+'error aya');
+    }
 })
+// update user
+
+app.patch('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body
+    try {
+        const user = await UserModel.findByIdAndUpdate({ _id: userId }, data, {
+            returnDocument: 'after',
+            runValidators: true,
+
+        });
+        res.send('user updated successfully')
+    } catch (err) {
+        res.status(400).send(err.message + ' update failed');
+    }
+});
+
+
 
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,62 @@
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minlength: 2,
+        maxLength: 50,
+
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
-    },
-    username: {
-        type: String,
+        required: true,
+        unique: true, // Ensure this is correct
+        lowercase: true,
+        trim: true,
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        required: true,
+        min: 18,
+        max: 100,
     },
     gender: {
         type: String,
+        validate(value) {
+            if (!['Male', 'Female', 'Other'].includes(value)) {
+                throw new Error('Gender must be Male, Female or Other');
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://via.placeholder.com/150"
+    },
+    about: {
+        type: String,
+        default: "This is a default"
+    },
+    skills: {
+        type: [String],
+        required: true,
+    },
     
+},
+    {
+        timestamps: true 
+        
     }
-})
+);
 
-module.exports = mongoose.model('User', userSchema);
+// Create the model
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
