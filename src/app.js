13de719ce4add6c19d973eec03519a60ff60f773
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2024-10-11 11:04:56 +0530

    corrected things

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use('/user',(req,res)=> {
+app.use('/', (req, res) => {
     //route handler
     res.send('Hello from user route');
-})
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     
